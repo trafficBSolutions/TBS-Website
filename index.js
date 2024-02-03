66276ea89b9a619b9f7ed11089bef46982e6b25f
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-02-03 12:48:13 -0500

    index.js

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const {mongoose} = require('mongoose');
-const app = express();
-
-//Import removeDuplicates function
+const { mongoose } = require('mongoose');
 const removeDuplicates = require('./controllers/duplicateHandler');
+
+// Create Express app
+const app = express();
 
 // Database connection
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log('Database Connected');
-    // Call removeDuplicates function after database connection
-    removeDuplicates();
-  })
-  .catch((err) => console.log('Database Not Connected', err));
+    .then(() => {
+        console.log('Database Connected');
+        // Call removeDuplicates function after database connection
+        removeDuplicates();
+    })
+    .catch((err) => console.log('Database Not Connected', err));
 
-  
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use('/', require('./routes/autoRoute'));
 
-const port = 8000;
+// Default route
+app.get("/", async (req, res) => {
+    res.send("Success!!!!!!");
+});
+
+// Define port
+const port = process.env.PORT || 8000;
+
+// Start server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
